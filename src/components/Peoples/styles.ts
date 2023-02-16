e07e6741df453c2d9cad06a626c891c20027d90b
MARCELO SANCHES OLIVEIRA <22102392@dsn127m1241959.cfp127aluno.local>
2023-02-16 08:20:08 -0300

    web

--- src/components/Peoples/styles.ts
+++ src/components/Peoples/styles.ts
@@ -2,17 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.section `
 
-padding: 6rem 1rem 1rem;
+display: flex;
+align-items: flex-end;
+background-color: beige;
+height: 75rem; 
+width: 80rem;
 
 `
 
 export const Content = styled.image `
 
+background-color: aliceblue;
     
-
      .peoples {
-    max-width:  42rem;
-    max-height: 32rem;
+    max-width:  60rem;
+    max-height: 50rem;
     width: auto;
     height: auto;
      }
